bbf37d5a8af8d754d4d959b1a70f8f71c8a66c93
LukePadawalker <padurariu.luca@gmail.com>
2024-02-21 19:31:47 +0200

    milestone 1 done

--- index.php
+++ index.php
@@ -1,3 +1,40 @@
+<?php
+
+function randomPasswordGenerator($length)
+{
+    $character = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_';
+    $character_length = strlen($character);
+    $password = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $number = rand(0, $character_length - 1);
+        $password .= $character[$number];
+    }
+
+    return $password;
+}
+
+// Verifica se il modulo è stato inviato
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['password_length'])) {
+    $password_length = intval($_GET['password_length']);
+
+    // Controlla se la lunghezza della password è valida
+    if ($password_length > 0) {
+        // Genera la password casuale
+        $password_casuale = randomPasswordGenerator($password_length);
+
+        // Mostra la password all'utente
+        echo '<p>La tua password casuale è: ' . htmlspecialchars($password_casuale) . '</p>';
+    } else {
+        echo '<p>La lunghezza della password deve essere un numero positivo maggiore di 0.</p>';
+    }
+}
+
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -19,8 +56,54 @@
             <div class="alert alert-danger d-flex justify-content-center align-items-center mb-4" role="alert">
                 Nessun parametro valido inserito!
             </div>
-            <div class="password-generator"></div>
+
+
+            <form class="password-generator d-flex flex-column justify-content-start p-5" action="index.php"
+                method="get">
+                <div class="row pt-4">
+                    <div class="col-6 d-flex justify-content-start ps-4">
+                        <p>Lunghezza password:</p>
+                    </div>
+                    <div class="col-6 d-flex justify-content-start ps-4"><input type="number" name="password_length">
+                    </div>
+                </div>
+                <div class="row pt-4">
+                    <div class="col-6 d-flex justify-content-start ps-4">
+                        <p>Consenti ripetizioni di uno o più caratteri:</p>
+                    </div>
+                    <div class="col-6 d-flex align-items-start flex-column ps-4">
+                        <label class="form-check-label" for="flexRadioDefault1"><input class="form-check-input mb-4"
+                                type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                            Sì</label>
+                        <label class="form-check-label" for="flexRadioDefault2"><input class="form-check-input mb-4"
+                                type="radio" name="flexRadioDefault" id="flexRadioDefault2">
+                            No</label>
+                    </div>
+                </div>
+                <div class=" row pt-4">
+                    <div class="col-6 d-flex justify-content-center">
+                    </div>
+                    <div class="col-6 d-flex align-items-stat flex-column ps-4">
+                        <label for="lettere"><input type="checkbox" name="lettere">
+                            Lettere</label>
+                        <label for="numeri"><input type="checkbox" name="numeri"> Numeri</label>
+                        <label for="simboli"><input type="checkbox" name="simboli"> Simboli</label>
+                    </div>
+                </div>
+                <div class="row pt-4">
+                    <div class="col-6 d-flex justify-content-start p-4">
+                        <input class="btn btn-primary me-2" type="submit" value="Invia">
+                        <input class="btn btn-primary me-2" type="submit" value="Annulla">
+                    </div>
+                    <div class="col-6 d-flex align-items-center flex-column">
+                    </div>
+                </div>
+            </form>
+
+
+
         </div>
+    </div>
 
     </div>
 
