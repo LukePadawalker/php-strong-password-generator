546ff9dce467ebe3e6643696a67c138d6c0893af
LukePadawalker <padurariu.luca@gmail.com>
2024-02-21 17:31:16 +0200

    added some style

--- index.php
+++ index.php
@@ -5,10 +5,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>Password generator</title>
 </head>
 
 <body>
+    <div class="main">
+        <div class="container d-flex flex-column align-items-center pt-4">
+            <div class="h1 pb-4 pt-4">Strong Password generator</div>
+            <div class="h1 pb-4">Genera una password sicura</div>
+            <div class="alert alert-danger d-flex justify-content-center align-items-center mb-4" role="alert">
+                Nessun parametro valido inserito!
+            </div>
+            <div class="password-generator"></div>
+        </div>
+
+    </div>
 
 </body>
 
